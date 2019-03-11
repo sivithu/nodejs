bdf9c595e09c6370a2cbb862ada7d2f4dd5bbe26
vithursan sivakumaran <sivithu93240@gmail.com>
2019-03-11 14:29:23 +0100

    add default port

--- serveur.js
+++ serveur.js
@@ -2,11 +2,13 @@ var express = require('express');
 
 var app = express();
 
+var port = process.env.PORT || 3000
+
 app.get("/hello", (req, res) => {
   console.log("Responding to root...");
   res.send("Hello World!");
 })
 
-app.listen(3000, () => {
-  console.log("Server is up and listnening on port 3000...");
+app.listen(port, () => {
+  console.log("Server is up and listnening on port " + port + "...");
 })

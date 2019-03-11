05fd675e133a11a57a1e0426c07b166bde41b82e
vithursan sivakumaran <sivithu93240@gmail.com>
2019-03-11 15:05:30 +0100

    serveur update 1.2

--- serveur.js
+++ serveur.js
@@ -9,6 +9,15 @@ app.get("/hello", (req, res) => {
   res.send("Hello World");
 })
 
+app.post("chat", (req,res) => {
+  var msg = req.msg;
+  if(msg == "ville") {
+    res.send("Nous sommes à Paris")
+  } else if(msg == "météo"){
+    res.send("Il fait beau")
+  }
+})
+
 app.listen(port, () => {
   console.log("Server is up and listnening on port " + port + "...");
 })

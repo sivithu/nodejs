d295450d75b9bc777de2ec6976c5d77878b943a8
vithursan sivakumaran <sivithu93240@gmail.com>
2019-03-12 16:33:20 +0100

    2.2

--- serveur.js
+++ serveur.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs')
 
 var app = express();
 
@@ -21,7 +22,31 @@ app.post("/chat", (req,res) => {
     res.send("Il fait beau")
     console.log("Il fait beau")
   }
-  console.log(msg);
+
+  if(msg != null){
+    var word = msg.split(" = ");
+    if(word[0] == "demain"){
+      if(word[1] != null){
+        var obj = []
+        obj.push({demain: word[1]});
+        fs.writeFileSync('reponses.json', JSON.stringify(obj))
+        console.log("Merci pour cette information")
+      } else {
+        //console.log("bad");
+        var file = fs.readFileSync('reponses.json')
+        var newFile = JSON.parse(file);
+        var key;
+        for (key in newFile) {
+          if (newFile.hasOwnProperty(key)) {
+              console.log(key + " = " + newFile[key]);
+          }
+        }
+
+      }
+    }
+  }
+
+
 })
 
 app.listen(port, () => {
